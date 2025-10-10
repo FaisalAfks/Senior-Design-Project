80ecbe91e9c53019bec3950340e5802cf0820453
FaisalAfks <Fmehmadi9@gmail.com>
2025-10-10 16:29:45 +0300

    Delete antispoof directory

--- antispoof/__init__.py
+++
@@ -1,7 +0,0 @@
-﻿from .antispoof_model import DeePixBiS
-from .antispoof_dataset import PixWiseDataset
-from .antispoof_loss import PixWiseBCELoss
-from .antispoof_metrics import predict, test_accuracy, test_loss
-from .antispoof_trainer import Trainer
-
-__all__ = ['DeePixBiS', 'PixWiseDataset', 'PixWiseBCELoss', 'predict', 'test_accuracy', 'test_loss', 'Trainer']
